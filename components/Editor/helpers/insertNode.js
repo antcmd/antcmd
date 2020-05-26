f5deb36d7c6d4f60aa928c406955065db89025cf
ignatif <ignatif@gmail.com>
2020-05-26 19:49:12 +0300

    remove Editor

--- components/Editor/helpers/insertNode.js
+++
@@ -1,9 +0,0 @@
-import { Transforms } from 'slate'
-
-export const insertNode = (editor, type, character) => {
-  const node = { type, character, children: [{ text: '' }] }
-  console.log('node')
-  console.log(node)
-  Transforms.insertNodes(editor, node)
-  Transforms.move(editor)
-}
