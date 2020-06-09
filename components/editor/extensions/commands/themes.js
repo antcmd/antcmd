c17bd5c9fa6e902789dbc20db711c540a7d88a67
ignatif <ignatif@gmail.com>
2020-06-09 16:04:19 +0300

    add themes

--- components/editor/extensions/commands/themes.js
+++ components/editor/extensions/commands/themes.js
@@ -1,28 +1,25 @@
 import { InputRule } from 'prosemirror-inputrules'
 import { Extension } from 'tiptap'
 
+function createThemeInputRule(theme, setTheme) {
+  return new InputRule(new RegExp(`/${theme}`), (state, match, start, end) => {
+    const htmlElement = document.documentElement
+
+    htmlElement.setAttribute('theme', theme)
+    setTheme(theme)
+
+    return state.tr.insertText('', end - (theme.length + 1), end)
+  })
+}
+
 export default class Themes extends Extension {
   inputRules({ type }) {
     return [
-      // Dark
-      new InputRule(new RegExp(`/dark`), (state, match, start, end) => {
-        const htmlElement = document.documentElement
-
-        htmlElement.setAttribute('theme', 'dark')
-        this.options.setTheme('dark')
-
-        return state.tr.insertText('', end - 5, end)
-      }),
-
-      // Light
-      new InputRule(new RegExp(`/light`), (state, match, start, end) => {
-        const htmlElement = document.documentElement
-
-        htmlElement.setAttribute('theme', 'light')
-        this.options.setTheme('light')
-
-        return state.tr.insertText('', end - 6, end)
-      }),
+      createThemeInputRule('dark', this.options.setTheme),
+      createThemeInputRule('light', this.options.setTheme),
+      createThemeInputRule('yellow', this.options.setTheme),
+      createThemeInputRule('red', this.options.setTheme),
+      createThemeInputRule('grey', this.options.setTheme),
 
       // Toggle
       new InputRule(new RegExp(`/theme`), (state, match, start, end) => {
